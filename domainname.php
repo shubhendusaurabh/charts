f57e635b3c414dc09883d93fc555b9defbc2a625
shubhu <shubhu@pumpndump.in>
2013-07-03 01:26:41 +0530

    Included html files and broken scraper.php

--- domainname.php
+++ domainname.php
@@ -1,15 +1,27 @@
 <?php
 	
-	$filename = 'http://bigrock.in';
-	$data = file_get_contents($filename);
+	//$filename = 'Libraries\Documents\IN Domain Registration & other Domain Registration Pricing List - BigRock.htm';
+	//$filename = 'BigRock.htm';
+	$filename = "http://www.bigrock.in/domain-registration/domain-registration-price.php";
+	$filename = "http://www.bigrock.in/";
+	$filename = "BigRock.htm";
+	$filename = "bigindex.htm";
+	//$data = file_get_contents($filename);
+	//var_dump($data);
+	//$data = 
+	$info = new ArrayObject;
 	$doc = new DOMDocument;
-	$doc->loadHTML($data);
+	$doc->loadHTMLFile($filename);
 	$errors = libxml_get_errors();
 	
 	$xpath = new DOMXPath($doc);
-	$lists = $xpath->query('/html/');
+	//$lists = $xpath->query('/html/body/div[4]/div/div[4]/div[3]/div[2]/div/div[4]/div[2]/div');
+	$lists = $xpath->query('/*[@class=“price-list”');
 	
 	foreach($lists as $list){
 		$info[] = $list->nodeValue;
+		echo $list->nodeValue;
 	}
+	
+	echo $errors;
 	var_dump($info);
---
+++ requirement.md
@@ -0,0 +1,16 @@
+Goals =>
+The project should do the following things
+1> should retrieve data weekly through CRON jobs
+2> should have an Admin interface
+3> should use d3.js or similar library[svg based]
+4> should fetch from multiple site
+--------------------------------------
+Problems
+=> can't decide how to scrap data xpath/css?
+=>how to retrieve data
+=>
+* need help regarding downloading
+# how do you do it
+---------------------------
+Steps to take 
+#1 First retrieve content from data i.e. scrap first
